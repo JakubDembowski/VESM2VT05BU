c17380ff6591d6e15b2d3c55e435ea012acc05ce
Jakub Dembowski <54886151+JakubDembowski@users.noreply.github.com>
2020-12-07 12:30:57 +0000

    Update LokaverkefniVESM.ino

--- Lokaverkefni/Kóði/LokaverkefniVESM.ino
+++ Lokaverkefni/Kóði/LokaverkefniVESM.ino
@@ -1,1 +1,29 @@
-
+#include <AccelStepper.h>
+#include <Servo.h>
+#define FULLSTEP 4
+AccelStepper myStepper(FULLSTEP,  8,  10,  9,  11);
+Servo myservo;   
+int pos  =  50;   
+bool direction  =  true; 
+void setup()  {
+    myStepper.setMaxSpeed(1000.0);
+    myStepper.setAcceleration(50.0);
+    myStepper.setSpeed(200);
+    myStepper.moveTo(400);
+    myservo.attach(7);   
+}
+void loop()  {
+    if  (myStepper.distanceToGo()  ==  0)
+    myStepper.moveTo(-myStepper.currentPosition());
+    myStepper.run();
+    if (direction) {
+        pos++;
+    } else  {
+        pos--;
+    }
+    if (pos  ==  50  ||  pos  ==  120) {
+        direction  =  !direction;
+    }
+    myservo.write(pos);
+    delay(15);
+}
